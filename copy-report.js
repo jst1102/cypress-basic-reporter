8c09f15b09e54bb24cdc79dcd858fd7683c0be5e
jst1102 <jsthomas1102@gmail.com>
2024-03-19 18:39:03 -0400

    removed ncp / update package
    removed ncp from copy-report.js
    updated package.json to newer version of cypress

--- copy-report.js
+++ copy-report.js
@@ -1,10 +1,23 @@
-const ncp = require('ncp').ncp;
 const path = require('path');
+const fs = require('fs');
 
 const sourceFile = path.join(__dirname, 'report.html');
-const destinationDir = path.join(__dirname, '..', 'reports', 'finalReports');
+const reportsDir = path.join(__dirname, '..', 'reports');
+const destinationDir = path.join(reportsDir, 'finalReports');
+const destinationFile = path.join(destinationDir, 'report.html');
 
-ncp(sourceFile, destinationDir, (err) => {
+// Create the 'reports' directory if it doesn't exist
+if (!fs.existsSync(reportsDir)) {
+  fs.mkdirSync(reportsDir, { recursive: true });
+}
+
+// Create the 'finalReports' directory if it doesn't exist
+if (!fs.existsSync(destinationDir)) {
+  fs.mkdirSync(destinationDir, { recursive: true });
+}
+
+// Copy the file
+fs.copyFile(sourceFile, destinationFile, (err) => {
   if (err) {
     console.error('Error copying report.html:', err);
     return;
